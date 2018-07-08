4a97d5fd0e2af7e582548b759dbf20217cb7ff36
Yehoshua Levin <ylevin.kw@gmail.com>
2018-07-08 17:03:10 -0400

    version 2

---
+++ BasketballGames/Scripts/site-scripts/add-game.js
@@ -0,0 +1,17 @@
+﻿$(() => {
+    $("#max-people").on('change', setButtonValidity);
+    $("#date").on('change', setButtonValidity);
+
+    function setButtonValidity() {
+        $("#submit-button").prop('disabled', !isFormValid());
+    }
+
+    function isFormValid() {
+        const maxPeople = $("#max-people").val();
+        const date = $("#date").val();
+        return maxPeople && date;
+    }
+
+})
+
+
---
+++ BasketballGames/Scripts/site-scripts/add-person.js
@@ -0,0 +1,20 @@
+﻿$(() => {
+    $("#name").on('keyup', setButtonValidity);
+    $("#email").on('keyup', setButtonValidity);
+
+    function setButtonValidity() {
+        $("#submit-button").prop('disabled', !isFormValid());
+    }
+
+    function isFormValid() {
+        const email = isValidEmail($("#email").val())
+        const name = $("#name").val();
+        return name && email;
+    }
+
+
+    function isValidEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+})
